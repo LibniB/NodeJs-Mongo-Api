18c289be9f29d475d2a3b1fe399d56335361ce6d
LibniB <136601868+LibniB@users.noreply.github.com>
2024-07-22 10:53:35 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ const maquinasRoutes = require("./routes/maquinas");
 const app = express();
 const port= process.env.PORT || 9000;
 
+// Add CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 //middleware
 app.use(express.json())
 app.use('/api',operariosRoutes);
