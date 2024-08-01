9cc0beb8b1760817263357227722a9251c4b7669
LibniB <136601868+LibniB@users.noreply.github.com>
2024-08-01 11:16:37 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express= require('express');
 const mongoose=require("mongoose");
-const cors = require('cors');
+
 require("dotenv").config();
 const operariosRoutes = require("./routes/operarios");
 const proveedoresRoutes= require("./routes/proveedores");
@@ -10,21 +10,21 @@ const usersRoutes = require("./routes/users");
 const app = express();
 const port= process.env.PORT || 9000;
 
-// Enable CORS for specific origins or all origins
-app.use(cors({
-  origin: 'http://localhost:50183', // Allow only this origin
-  // origin: '*', // Allow all origins
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
-  credentials: true // Allow credentials (cookies, etc.)
-}));
-// Add CORS middleware
-//app.use((req, res, next) => {
-//res.header('Access-Control-Allow-Origin', '*');
-//res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//next();
-//});
 
+// Add CORS middleware
+app.use((req, res, next) => {
+res.header('Access-Control-Allow-Origin', '*');
+res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+next();
+});
+
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.sendStatus(200);
+});
 //middleware
 app.use(express.json())
 app.use('/api',operariosRoutes);
